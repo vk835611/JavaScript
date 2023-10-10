d7e7688040377d757b2195f40a71b7cce1c9ff07
vk835611 <107438833+vk835611@users.noreply.github.com>
2023-10-10 07:51:39 +0000

    basic of arrays

--- basic/arrary.js
+++ basic/arrary.js
@@ -5,6 +5,10 @@ console.log((arr1 + arr2));
 
 // const all_num = arr1.concat(arr2)
 
-console.log(all_num);
+// console.log(all_num);
 const all_num = [...arr1,...arr2]
 console.log(all_num);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+console.log(another_array.flat(Infinity));
