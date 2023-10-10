7926c90db0c26cee3a3162dba2cdccdcd9e93e2e
vk835611 <107438833+vk835611@users.noreply.github.com>
2023-10-10 07:46:19 +0000

    conf

---
+++ basic/arrary.js
@@ -0,0 +1,10 @@
+const arr1 = [1,2,3,4,5]
+const arr2 = [6,7,8,9,10]
+
+console.log((arr1 + arr2)); 
+
+// const all_num = arr1.concat(arr2)
+
+console.log(all_num);
+const all_num = [...arr1,...arr2]
+console.log(all_num);
