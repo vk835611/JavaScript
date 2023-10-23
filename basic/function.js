6cca7af1e192f24df32202d6b611a43dc4b78ae8
vk835611 <107438833+vk835611@users.noreply.github.com>
2023-10-23 19:16:45 +0000

    IIfe

--- basic/function.js
+++ basic/function.js
@@ -8,3 +8,44 @@
 //     return (num1 + num2)
 // }
 // addtwoNumber(5,8)
+
+// function calculateCartPrice(...num1){ // ...num1 is used as rest operater it also uesd as sparead operator.
+//     return num1
+// }
+// console.log(calculateCartPrice(10,  5000, 80001));
+/*
+this keyword are refer the current contest or value of any object or globaly 
+
+NOTE => In node environment this keyword refer to the empty object but in bowser it refer 
+        to window object which capture the event of the window.
+     
+*/
+
+// const addTwo = (num1, num2) =>{
+//     return num1 + num2
+// }
+
+
+
+// const addTwo = (num1, num2) =>(num1 + num2)
+// console.log(addTwo(4,7));
+
+/* Immediately Invoked Function Expressions (IIFE):      
+    Is a function that is called immediately after it is defined.
+*/
+
+// (function doSum(){
+        // name IIFE
+//     console.log(`DB CONNECTION`);
+// })();
+
+
+// ((database) => {
+//     unname IIFE
+//     console.log(`DB CONNECTION ${database}`);
+// })('RDBMS');
+
+                        //      Javascript Execution Context  ===>  see on eraser.com
+
+
+                        
